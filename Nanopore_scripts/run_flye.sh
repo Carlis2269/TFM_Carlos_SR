bc58f9be343d955b47a120532f0d1c160ffe597c
Carlis <carlossellesribera@gmail.com>
2025-04-15 15:12:20 +0200

    Scripts Flye

---
+++ Nanopore_scripts/run_flye.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+input_dir=$1
+output_dir=$2
+mkdir -p "$output_dir"
+
+for trimmed_file in "$input_dir"/*_trimmed.fastq; do
+    base_name=$(basename "${trimmed_file%_trimmed.fastq}")
+    output_subdir="${output_dir}/${base_name}"
+    mkdir -p "$output_subdir"
+
+    flye --nano-raw "$trimmed_file" \
+         --out-dir "$output_subdir" \
+         --threads 8 \
+         --genome-size 5m \
+done
