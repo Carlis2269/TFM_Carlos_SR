b9a6954575b15a08c7b3f3806a0f473eb63501ba
Carlis2269 <carlossellesribera@gmail.com>
2025-06-04 19:56:30 +0200

    Remove unused files

--- Nanopore_scripts/run_nanoqc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-input_dir=$1 # Directory containing FASTQ files
-output_dir=$2 # Directory to save NanoQC reports
-mkdir -p "$output_dir" # Create output directory if it doesn't exist
-
-for fastq_file in "$input_dir"/*.fastq; do # Loop through each FASTQ file
-    nanoQC "$fastq_file" -o "$output_dir" # Run NanoQC on the FASTQ file
-done
